c8bde4bdbf203f78f8890433f0a756cb96602b83
Thomas Marques <thomas.marques@edu.devinci.fr>
2024-12-13 12:32:39 +0100

    ✨ Suppression de l'importation inutile de useEffect dans le composant CardShine pour alléger le code.

--- src/app/components/CardShine.tsx
+++ src/app/components/CardShine.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Image from "next/image";
 import { PokemonItem } from "../../store/PokemonStore";
 import { usePokemonStore } from "../../store/PokemonStore";
